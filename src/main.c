47b2dd48d653577012add59f555c94ad9ab43124
Gabryx64 <gabryx86_64@riseup.net>
2023-05-29 07:54:36 +0200

    -C

--- src/main.c
+++ src/main.c
@@ -9,9 +9,7 @@ int main(void) {
   consoleInit();
   gfx_init();
 
-  for (i = 0; i < 128; i++) {
-    init_sprite(i * 4, (i % 16) * 16, i / 16 * 16, 3, 0, 0, i, 0);
-  }
+  init_sprite(0, 100, 50, 3, 0, 0, 0, 0);
 
   while (1) {
     flush();
